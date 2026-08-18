8ac00df92cde6fe07d21e717e05e3668b0810cbd
UnforGBeast <atahusain123@gmail.com>
2026-08-18 12:36:46 +0530

    redirected the admin to main, and scanner to scanner section

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.scanner_ui, name='scanner_ui'),
+    path('scanner/', views.scanner_ui, name='scanner_ui'),
     path('api/inbound/', views.log_inbound, name='log_inbound'),
     path('api/outbound/', views.log_outbound, name='log_outbound'),
     path('api/return/', views.log_return, name='log_return'),
